2bbc970698eb685a5c2e59f9c9bcf2846ae577a5
qiuqiqixang <739140860@qq.com>
2022-12-01 16:20:14 +0800

    -changed：文件修改

--- Example/WZUIExtension/ViewController.swift
+++ Example/WZUIExtension/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        debugPrint("添加fastlane")
     }
 
     override func didReceiveMemoryWarning() {
